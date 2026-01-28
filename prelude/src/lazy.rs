0d27fe905d2da860d66c5ffa2363d75c9d149b62
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-01-28 18:28:37 +0100

    optimized Lazy for inlined deref

--- prelude/src/lazy.rs
+++ prelude/src/lazy.rs
@@ -3,24 +3,37 @@ use once_cell::race::OnceBox;
 
 use crate::*;
 
+/// A lazy initializer, to use for static validators.
+/// It functions as a wrapper to [`OnceBox`](once_cell::race::OnceBox) that initializes at first deref like `LazyLock`.
 pub struct Lazy<T, F = fn() -> T> {
   cell: OnceBox<T>,
   init: F,
 }
 
-impl<T, F> Lazy<T, F> {
+impl<T, F> Lazy<T, F>
+where
+  F: Fn() -> T,
+{
+  /// Creates a new instance.
   pub const fn new(f: F) -> Self {
     Self {
       cell: OnceBox::new(),
       init: f,
     }
   }
+
+  #[inline(never)]
+  #[cold]
+  fn start(&self) -> Box<T> {
+    Box::new((self.init)())
+  }
 }
 
 impl<T, F: Fn() -> T> Deref for Lazy<T, F> {
   type Target = T;
 
+  #[inline]
   fn deref(&self) -> &T {
-    self.cell.get_or_init(|| Box::new((self.init)()))
+    self.cell.get_or_init(|| self.start())
   }
 }
